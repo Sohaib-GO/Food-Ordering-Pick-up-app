c2808bfd1e3774dea5595fa31441dab9ebad8d39
Njoe00 <nick.james.joe@gmail.com>
2023-01-03 22:02:47 +0000

    creating html that returns users info from db

--- routes/account.js
+++ routes/account.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const usersDb = require("../db/queries/hard-db");
 
 router.get('/', (req, res) => {
-    return res.render('acccount');
+    const user_id = req.session.user_id;
+    const username = usersDb[user_id];
+    const templateVars = {username, usersDb, user_id};
+    
+    if(!username) {
+        res.redirect('/homepage');
+    }
+    
+    return res.render('account', templateVars);
 });
 
 module.exports = router;
