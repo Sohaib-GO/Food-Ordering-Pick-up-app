0f685918ae8d9b92f981585f642dd1c8aa0209ff
Sohaib-GO <s.almajmaie@gmail.com>
2023-01-03 18:43:57 -0700

    Add order function
    working on the function to add menu items to users order

--- public/scripts/menu-items.js
+++ public/scripts/menu-items.js
@@ -8,13 +8,16 @@ export function displayMenu(menuItems) {
     const $name = $(`<h4 class="name">${item.food_name}</h4>`);
     const $description = $(`<p>${item.food_description}</p>`);
     const $price = $(`<h2 class="price">$${item.price}</h2>`);
-    const $orderButton = $(`<a href="#">Order Now</a>`);
-    const $btn = $(`<div class="btn">`).append($price, $orderButton);
+    const $orderButton = $(`<a class="order-button" href="#">Order Now</a>`);
+    const $btn = $(`<div class="btn">  id="order-button-${item.id}`).append($price, $orderButton);
     const empty = $(`<div class="empty">`);
 
     $contentBox.append($name, $description, $btn);
     $menuContainer.append($img, $contentBox);
     $menuContainer.append(empty);
     $("#menu-items").append($menuContainer);
+
+
+    
   }
 }
