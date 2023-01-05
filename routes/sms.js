4adda541455202ef4a3ab420886eac097d91797f
Njoe00 <nick.james.joe@gmail.com>
2023-01-05 03:11:53 +0000

    creating testing db and creating route where clicking on check out will send sms with user info

--- routes/sms.js
+++ routes/sms.js
@@ -1,26 +1,39 @@
 require('dotenv');
 const accountSid = process.env.ACace56e4e5f37db9bbc0900521773a3e5;
-const authToken = process.env.c26ba9741aaf769fb2023ddb30195897;
+const authToken = process.env.d667f6a1f1aeeb898751081b887b08b3;
 const client = require('twilio')(accountSid, authToken);
 const express = require("express");
 const router = express.Router();
+const ordersDb = require('../db/queries/hard_order-db');
+const orderNum = 'userRandomID'
 
-$(document).ready(function() {
+const getOrderById = (orderNum, ordersDb) => {
+    for (let id in ordersDb) {
+        if (orderNum === ordersDb[id].order_id) {
+            return true; 
+        }
+    }
+};
+
+router.post('/', (req, res) => {
     
-    $("menu-checkout-btn").submit(function(event) {
-        
-        event.preventDefault();
-
-        client.messages.create({  
-                body: `test4321`,
+    if (getOrderById) {
+        for (let id in ordersDb) {
+            let test = ordersDb[id].order_id;
+            client.messages.create({  
+                body: test,
                 from: '+17154024150',
-                to: '+16046003716'
+                to: '+16046003716' 
             })
-            .then(message => console.log(message.sid))
-            .catch(err => console.log(err));
-    })
+        .then(message => console.log(message.sid))
+        .catch(err => console.log(err));
+        }
+    }
+    res.redirect('/homepage');
 });
 
+
+       
 
 
 module.exports = router;
